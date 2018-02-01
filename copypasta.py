ab93948e42f0e3c8616346bfcc832c58142d3d11
FelixSFD <felixsfd@sobotics.org>
2018-02-01 16:36:26 +0100

    #10: - fimade username optional

--- copypasta.py
+++ copypasta.py
@@ -35,10 +35,15 @@ def store_post():
         date_two = request.form["date_two"]
         if date_one < date_two:
             return jsonify({"status": "failure", "message": "Error - Plagiarized post created earlier"}), 400
-        data = (request.form["url_one"], request.form["url_two"], request.form["title_one"],
+        
+        if request.form.get('username_one', None) is not None:
+            data = (request.form["url_one"], request.form["url_two"], request.form["title_one"],
                 request.form["title_two"], date_one, date_two, request.form["body_one"], request.form["body_two"],
                 request.form["username_one"], request.form["username_two"], request.form["user_url_one"],
                 request.form["user_url_two"])
+        else:
+            data = (request.form["url_one"], request.form["url_two"], request.form["title_one"],
+                request.form["title_two"], date_one, date_two, request.form["body_one"], request.form["body_two"])
         post_id = save_data(data)
         if post_id == -1:
             return jsonify({"status": "failure", "message": "Error - Post already present"}), 400
@@ -176,9 +181,16 @@ def save_data(data):
         cur.execute("SELECT  * FROM posts WHERE url_one=? AND url_two=?;", (data[0], data[1]))
         if cur.fetchone():
             return -1
-        cur.execute("INSERT INTO posts "
+        
+        if len(data) != 12:
+            cur.execute("INSERT INTO posts "
                     "(url_one, url_two, title_one, title_two, date_one, date_two, body_one, body_two, username_one, username_two, user_url_one, user_url_two) "
-                    "VALUES (?,?,?,?,?,?,?,?,?,?,?,?);", data)
+                    "VALUES (?,?,?,?,?,?,?,?,'','','','');", data)
+        else:
+            cur.execute("INSERT INTO posts "
+                        "(url_one, url_two, title_one, title_two, date_one, date_two, body_one, body_two, username_one, username_two, user_url_one, user_url_two) "
+                        "VALUES (?,?,?,?,?,?,?,?,?,?,?,?);", data)
+                        
         cur.execute("SELECT last_insert_rowid();")
         post_id = cur.fetchone()[0]
         db.commit()
