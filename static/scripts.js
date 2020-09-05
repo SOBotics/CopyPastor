7e20f5afc29c5481fc124a1d138995cfb1b1f6de
double-beep <doublebeep7@gmail.com>
2020-09-05 09:15:10 +0000

    Indicate post deletion with JavaScript

--- static/scripts.js
+++ static/scripts.js
@@ -72,3 +72,22 @@ diff.forEach(function(part){
 left.appendChild(left_fragment);
 right.appendChild(right_fragment);
 }
+
+
+// add (deleted) if a post has been deleted
+const apiKey = 'YNIhT*kUiXt*Rp*pDBTBnA((';
+const filter = '!-.HCX)kdt*(H';
+const sitename = 'stackoverflow';
+const apiUrl = 'https://api.stackexchange.com/2.2/posts/';
+const plagiarisedPostAnchor = document.querySelector('h2 a');
+const plagiarisedPostId = plagiarisedPostAnchor.href.split('/')[4];
+const originalPostAnchor = document.querySelectorAll('h2 a')[1];
+const originalPostId = originalPostAnchor.href.split('/')[4];
+const postIds = `${plagiarisedPostId};${originalPostId}`;
+const deletedSpan = ' <span class="text-danger">(deleted)</span>';
+
+fetch(`${apiUrl}${postIds}?site=${sitename}&filter=${filter}&key=${apiKey}`).then(response => response.json()).then(jsonResponse => {
+    const findPostId = id => jsonResponse.items.find(item => item.post_id == id);
+    if (!findPostId(plagiarisedPostId)) plagiarisedPostAnchor.parentElement.insertAdjacentHTML('afterend', deletedSpan);
+    if (!findPostId(originalPostId)) originalPostAnchor.parentElement.insertAdjacentHTML('afterend', deletedSpan);
+})
